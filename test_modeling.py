5f10e05252bb066dd2f6a383f60bdecfa9babb8d
Faraz M. Awan <faraz.awan0406@gmail.com>
2025-04-10 12:04:54 +0100

    Incorporated ML Training - Added the functionality of training ML models (Decision Tree and Random Forest) - Now the module is capable of performing data engineering and training and saving ML model. - gitignore has been modified

--- test_modeling.py
+++ test_modeling.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-from meowmotion.model_tmd import processTrainingData
+from meowmotion.model_tmd import trainMLModel
 
-proc_data = pd.read_csv(
-    "D:/Mobile Device Data/TMD_repo/ML_model/data/final_processed_data_ready_to_use.csv"
-)
-stat_df, val_stat_df = processTrainingData(proc_data)
+# proc_data = pd.read_csv(
+#     "D:/Mobile Device Data/TMD_repo/ML_model/data/final_processed_data_ready_to_use.csv"
+# )
+# stat_df, val_stat_df = processTrainingData(proc_data)
+# stat_df.to_csv('data/stat_df.csv', index=False)
+# val_stat_df.to_csv('data/val_stat_df.csv', index=False)
 
-print(stat_df.head())
-print(val_stat_df.head())
+stat_df = pd.read_csv("data/stat_df.csv")
+val_stat_df = pd.read_csv("data/val_stat_df.csv")
+
+trainMLModel(df_tr=stat_df, df_val=val_stat_df, model_name="RandomForest")
